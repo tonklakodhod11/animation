60b7d4eebf4fe67878dced64c0bd01adf5dbaf2d
tonklakodhod11 <s40596@bj.ac.th>
2024-11-22 15:22:28 +0700

    Add files via upload

---
+++ a2_direction_11/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1732263682,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/collide-sheet0.png",
+		"images/playeranimation-sheet0.png",
+		"images/playeranimation-sheet1.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
