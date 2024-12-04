38d5bcaf7d0a33f9914b66186c35eeffb7a1122c
tonklakodhod11 <s40596@bj.ac.th>
2024-12-04 13:28:44 +0700

    Add files via upload

---
+++ a3_sheep_11/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1733293529,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg-sheet0.png",
+		"images/player-sheet0.png",
+		"images/pillar.png",
+		"images/land-sheet0.png",
+		"images/line.png",
+		"images/point-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
