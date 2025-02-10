98a56ba81959ff58ea9bf53567301c5a89fb4b10
tonklakodhod11 <s40596@bj.ac.th>
2025-02-10 16:06:18 +0700

    Add files via upload

---
+++ a6_shooter_11/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1739178403,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/bg.png",
+		"images/player-sheet0.png",
+		"images/monster-sheet0.png",
+		"images/bullet-sheet0.png",
+		"images/explode-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
